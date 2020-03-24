6568dfd2c34599f56c60c7c2899f15e5f387b797
c1p81 <lucainnoc@gmail.com>
2020-03-24 14:07:09 +0100

    Long run test

--- app/src/main/java/com/luca/innocenti/autoboot/MainActivity.kt
+++ app/src/main/java/com/luca/innocenti/autoboot/MainActivity.kt
@@ -1,4 +1,22 @@
 package com.luca.innocenti.autoboot
+/*
+Script in php per il lato server
+<?php
+$tempo = $_GET['tempo'];
+$pitch = $_GET['pitch'];
+$roll = $_GET['roll'];
+$azimuth = $_GET['azimuth'];
+$batteria = $_GET['batteria'];
+
+//print $pitch + ";" + $roll + ";" + $azimuth;
+
+$fp = fopen('sensore.txt', 'a');
+fwrite($fp, $tempo.";".$pitch.";".$roll.";".$azimuth.";".$batteria.PHP_EOL);
+fclose($fp);
+
+?>
+*/
+
 
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -32,7 +50,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var azimuth_mean: Float = 0.0f
     private var pitch_mean: Float = 0.0f
     private var roll_mean: Float = 0.0f
-    private var max_itera: Int = 20000
+    private var max_itera: Int = 20000 // il dato viene inviato al server quando e' cumulato
+                                       // questo numero di misure
     private var batteryperc: Int = 0
     private var azimuth: Float= 0.0f
     private var pitch: Float= 0.0f
@@ -49,23 +68,23 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        // Gestore dello stato della batteria
         val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         this.registerReceiver(batteria, intentFilter)
+        //-------------------------
 
+        // Gestore accelerometro
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        //--------------------------
 
-
+        // Wake Lock
         val wakeLock: PowerManager.WakeLock =
             (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
                 newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::MyWakelockTag").apply {
                     acquire()
                 }
             }
-
-    
 
 
     }
@@ -87,14 +106,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             pitch = event.values[1]
             roll = event.values[2]
 
+            // calcola il valore medio
             azimuth_mean = azimuth_mean + azimuth
             pitch_mean = pitch_mean + pitch
             roll_mean = roll_mean + roll
 
             if (conteggio > max_itera) {
-
                 conteggio = 0
-                //val queue = Volley.newRequestQueue(this)
 
                 val currentDate = SimpleDateFormat("yyyy/MM/dd_HH:mm:ss", Locale.getDefault()).format(Date())
 
@@ -102,12 +120,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 roll_mean = roll_mean/max_itera
                 azimuth_mean = azimuth_mean/max_itera
 
-                val url = "http://192.168.1.102/index.php?tempo="+currentDate+"&pitch="+pitch_mean.toString()+"&roll="+roll_mean.toString()+"&azimuth="+azimuth_mean.toString()+"&batteria="+batteryperc.toString()
+                val url = "http://150.217.73.108/autoboot/index.php?tempo="+currentDate+"&pitch="+pitch_mean.toString()+"&roll="+roll_mean.toString()+"&azimuth="+azimuth_mean.toString()+"&batteria="+batteryperc.toString()
 
                 pitch_mean = 0.0f
                 roll_mean = 0.0f
                 azimuth_mean = 0.0f
 
+                // Invio al server in modalita' GET con la libreria FUEL
                 val httpAsync = url
                     .httpGet()
                     .responseString { request, response, result ->
@@ -124,24 +143,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                     }
 
                 httpAsync.join()
+                //-------------------------------------
 
                 Log.d("Volley",url)
-                //val stringRequest = StringRequest(Request.Method.GET, url,
-                //    Response.Listener { response ->
-                //        Log.d("Volley", "Response is: ${response}")
-                //    },
-                //    Response.ErrorListener { Log.d("Volley", "No")})
-
-                //val retryPolicy: RetryPolicy = DefaultRetryPolicy(
-                //    DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, //30000
-                //    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                //    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
-                //)
-                //stringRequest.setRetryPolicy(retryPolicy);
-
-                //queue.add(stringRequest)
             }
-            //Log.d("acc", conteggio.toString()+" "+azimuth.toString())
         }
     }
 
@@ -157,7 +162,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
 }
 
-
+// Gestite l'avvio della app al boot del device
 class BootReceiver : BroadcastReceiver() {
     private lateinit var mSensorManager: SensorManager
     private var mSensors: Sensor? = null
